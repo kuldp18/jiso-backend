b5fd5d61fe201ae28b68e860cee0b73d8285b04a
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-03-06 12:58:11 +0530

    implement add struggles as list route

--- controllers/struggle.controller.js
+++ controllers/struggle.controller.js
@@ -38,8 +38,55 @@ export const addStruggle = async (req, res) => {
   }
 };
 
-// add struggles list
-export const addStruggles = async (req, res) => {};
+// add struggles as list
+export const addStruggles = async (req, res) => {
+  const { struggles } = req.body;
+  try {
+    if (!struggles || struggles.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "A non-empty `struggles` array is required to add",
+      });
+    }
+
+    if (!Array.isArray(struggles)) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "`struggles` should be an array with at least one struggle item",
+      });
+    }
+
+    const userContext = await UserContext.findOne({ userId: req.userId });
+
+    if (!userContext) {
+      return res.status(400).json({
+        success: false,
+        message: "Couldn't find a context for this user",
+      });
+    }
+
+    const userStruggles = userContext.struggles;
+    const newStruggles = [...userStruggles, ...struggles];
+
+    userContext.struggles = newStruggles;
+
+    const updatedContext = await userContext.save();
+
+    res.status(200).json({
+      success: true,
+      message: "New struggles successfully added in the context",
+      context: updatedContext,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message:
+        error.message ||
+        "Something went wrong while adding these struggles in the context",
+    });
+  }
+};
 // fetch all struggles
 export const fetchAllStruggles = async (req, res) => {};
 // fetch single struggle
--- routes/userContext.route.js
+++ routes/userContext.route.js
@@ -18,7 +18,10 @@ import {
   editGoal,
 } from "../controllers/goal.controller.js";
 
-import { addStruggle } from "../controllers/struggle.controller.js";
+import {
+  addStruggle,
+  addStruggles,
+} from "../controllers/struggle.controller.js";
 
 const router = Router();
 
@@ -40,6 +43,7 @@ router.patch("/goal/edit/:goalId", editGoal); // edit goal
 
 // Struggles
 router.post("/struggle/add", addStruggle); //add single struggle
+router.post("/struggles/add", addStruggles); //add multiple struggles
 
 // Create journal and mood themes (for AI)
 router.post("/ai/theme/journal", createJournalTheme);
