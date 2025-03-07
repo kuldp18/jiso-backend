8712f362f3a0a100247c4a490ee56ec98b4bf0f6
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-03-07 15:31:30 +0530

    implement route to fetch all user mood entries

--- controllers/mood.controller.js
+++ controllers/mood.controller.js
@@ -1,6 +1,7 @@
 import { Mood } from "../models/mood.model.js";
 import mongoose from "mongoose";
 
+// create new mood entry
 export const createMoodEntry = async (req, res) => {
   let { emotions, description } = req.body;
 
@@ -37,6 +38,33 @@ export const createMoodEntry = async (req, res) => {
       success: false,
       message:
         error.message || "Something went wrong while create a new mood entry",
+    });
+  }
+};
+
+// get all mood entries
+export const fetchMoodEntries = async (req, res) => {
+  try {
+    const moodEntries = await Mood.find({ userId: req.userId });
+
+    if (moodEntries.length === 0) {
+      return res.status(404).json({
+        success: true,
+        message: "No mood entries found for this user",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Mood entries fetched successfully",
+      total: moodEntries.length,
+      entries: moodEntries,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message:
+        error.message || "Something went wrong while fetching mood entries",
     });
   }
 };
--- routes/mood.route.js
+++ routes/mood.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { createMoodEntry } from "../controllers/mood.controller.js";
+import {
+  createMoodEntry,
+  fetchMoodEntries,
+} from "../controllers/mood.controller.js";
 
 const router = Router();
 
 // Apply authentication middleware to all routes
 router.use(verifyToken);
 
-router.post("/create", createMoodEntry);
+router.post("/create", createMoodEntry); // create new mood entry
+router.get("/", fetchMoodEntries); // get all mood entries
 
 export default router;
