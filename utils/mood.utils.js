91ffc859b569b4cf13e2e1b561695f7720c1b2fd
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-03-10 16:31:21 +0530

    util for fetching last week moods

---
+++ utils/mood.utils.js
@@ -0,0 +1,30 @@
+import { Mood } from "../models/mood.model.js";
+
+// fetch last week moods
+export const fetchLastWeekMoods = async (userId) => {
+  try {
+    const lastWeekMoods = await Mood.find({
+      userId,
+      createdAt: {
+        $gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    });
+
+    if (!lastWeekMoods) {
+      return [];
+    }
+
+    // parse moods. each mood has emotions array and optional description
+    const parsedMoods = lastWeekMoods.map((mood) => {
+      return {
+        emotions: mood.emotions.join(", "),
+        description: mood.description || "none",
+      };
+    });
+
+    return parsedMoods;
+  } catch (error) {
+    console.error("Error fetching last week moods: ", error);
+    return [];
+  }
+};
